79ddfc678a88a9f2191ceda946dbd307b29b256b
kayh09 <h.kayleigh@wustl.edu>
2025-11-26 16:49:45 -0500

    imported usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-
-function Square({value}) {
+import { useState } from 'react';
+function Square() {
+  const [value, setValue] = useState(null);
   function handleClick() {
     console.log('clicked!');
   }
