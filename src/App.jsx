aab65457512eb7a4fccb7289e7fa2af6cdebde17
kayh09 <h.kayleigh@wustl.edu>
2025-11-26 16:09:25 -0500

    Added 9 squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import './App.css'
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+   <>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+
+  </>
+  );
 }
